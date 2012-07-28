f6c941577c9f6e07b7192fbf97c30bd82f884925
Michael Palumbo <michael.palumbo87@gmail.com>
2012-07-28 22:51:58 +0200

    Added test cases for serializers

---
+++ feedstorage/tests/utils/serializers.py
@@ -0,0 +1,36 @@
+#Django
+from django.test import TestCase
+
+from ...utils.serializers import serialize_function, deserialize_function
+
+
+def func1(a):
+    return a
+
+
+class A(object):
+
+    @classmethod
+    def classmethod1(cls, a):
+        return a
+
+
+class SerializationDeserializationKnownValues(TestCase):
+
+    def test_function(self):
+        f = func1
+        ser_f = serialize_function(f)
+        g = deserialize_function(ser_f)
+        v_a = f(9)
+        v_b = g(9)
+
+        self.assertEqual(v_a, v_b)
+
+    def test_classmethod(self):
+        f = A.classmethod1
+        ser_f = serialize_function(f)
+        g = deserialize_function(ser_f)
+        v_a = f(9)
+        v_b = g(9)
+
+        self.assertEqual(v_a, v_b)
