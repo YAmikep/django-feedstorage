f506aeb6d82d3595abaa3080b106ec3b26e95a4f
Michael Palumbo <michael.palumbo87@gmail.com>
2012-07-28 22:49:41 +0200

    Updated hub (docs + typo)

--- feedstorage/hub.py
+++ feedstorage/hub.py
@@ -1,6 +1,3 @@
-# Django
-from django.utils.translation import ugettext as _
-
 # Internal
 import models
 from .log import default_logger as logger
@@ -8,19 +5,24 @@ from .log import default_logger as logger
 
 class Hub(object):
     """Interface to use the feed storage."""
-    log_desc = _('[Hub]')
+    log_desc = '[Hub]'
 
     @classmethod
     def subscribe(cls, feed_url, callback, dispatch_uid=None):
-        """Subscribes to a feed to get notified of new entries."""
+        """Subscribes callback to a feed to get notified of new entries.
+        The susbscription is loaded and ready right away.     
+        
+        Returns:
+            Boolean to tell whether something goes wrong. 
+        """
 
-        log_desc = _('%(log_desc)s - Subscribing to %(feed_url)s') % {'log_desc': cls.log_desc, 'feed_url': feed_url}
+        log_desc = '%s - Subscribing to %s' % (cls.log_desc, feed_url)
 
         # Get or create the Feed
         f, created = models.Feed.objects.get_or_create(url=feed_url)
 
-        callback = models.Subscription.create_callack(callback)
-        dispatch_uid = models.Subscription.create_dispatch_uid(dispatch_uid, callback)
+        callback = models.Subscription.prepare_callback(callback)
+        dispatch_uid = models.Subscription.prepare_dispatch_uid(dispatch_uid, callback)
 
         try:
             # Get or create the subscription
@@ -31,29 +33,30 @@ class Hub(object):
             )
 
             if sub_created:
-                logger.info(_('%(log_desc)s => <Subscription: %(subscription)s> created') % {'log_desc': log_desc, 'subscription': sub})
+                logger.info('%s => <Subscription: %s> created' % (log_desc, sub))
 
             # Load it
             sub.load()
             return True
 
         except Exception as e:
-            logger.error(_('%(log_desc)s => Cannot get or create a Subscription: callback=%(callback)s (dispatch_uid=%(dispatch_uid)s) [KO]\n%(error)s') % {
-                'log_desc': log_desc,
-                'callback': callback,
-                'dispatch_uid': dispatch_uid,
-                'error': e
-            })
+            logger.error('%s => Cannot get or create a Subscription: callback=%s (dispatch_uid=%s) [KO]\n%s' % (
+                    log_desc,
+                    callback,
+                    dispatch_uid,
+                    e
+                )
+            )
             return False
 
     @classmethod
     def unsubscribe(cls, feed_url, callback, dispatch_uid=None):
-        """Unsubscribes to a Feed to not be notified anymore about new entries."""
+        """Unsubscribes callback to a Feed to not be notified anymore about new entries."""
 
-        log_desc = _('%(log_desc)s - Unsubscribing to %(feed_url)s') % {'log_desc': cls.log_desc, 'feed_url': feed_url}
+        log_desc = '%s - Unsubscribing to %s' % (cls.log_desc, feed_url)
 
-        callback = models.Subscription.create_callack(callback)
-        dispatch_uid = models.Subscription.create_dispatch_uid(dispatch_uid, callback)
+        callback = models.Subscription.prepare_callback(callback)
+        dispatch_uid = models.Subscription.prepare_dispatch_uid(dispatch_uid, callback)
 
         try:
             # Delete the subscription
@@ -63,14 +66,15 @@ class Hub(object):
                     dispatch_uid=dispatch_uid
                 )
             sub.delete()
-            logger.info(_('%(log_desc)s => <Subscription: %(subscription)s> deleted') % {'log_desc': log_desc, 'subscription': sub})
+            logger.info('%s => <Subscription: %s> deleted' % (log_desc, sub))
             return True
 
         except Exception as e:
-            logger.error(_('%(log_desc)s => Subscription cannot be deleted: callback=%(callback)s (dispatch_uid=%(dispatch_uid)s) [KO]\n%(error)s') % {
-                'log_desc': log_desc,
-                'callback': callback,
-                'dispatch_uid': dispatch_uid,
-                'error': e
-            })
+            logger.error('%s => Subscription cannot be deleted: callback=%s (dispatch_uid=%s) [KO]\n%s' % (
+                    log_desc,
+                    callback,
+                    dispatch_uid,
+                    e
+                )
+            )
             return False
